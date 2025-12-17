feacc078609142e266c7493a2bcdba2adf66e271
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2025-12-17 13:02:12 +0330

    refactor: change an input label

--- src/components/formatsInput/data.ts
+++ src/components/formatsInput/data.ts
@@ -292,7 +292,7 @@ export const inputsByDivision = {
 		// 1 Promotion Notice
 		{
 			name: "recipientName",
-			label: "Deputy full Rank and Name",
+			label: "Deputy full Name/Rank (or both)",
 			type: "text",
 			formats: ["1", "2", "3", "4", "5", "6", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
 		},
